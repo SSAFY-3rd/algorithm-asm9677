157adc8df3b2ed1bcdb24437d60b564f36d57fa0
SungMin Ahn <asm9677@naver.com>
2021-01-02 22:16:06 +0900

    2021-01-02 알고리즘 문제 풀이

--- g4/2643_색종이올려놓기.py
+++ g4/2643_색종이올려놓기.py
@@ -1,18 +1,7 @@
-import sys
-input = sys.stdin.readline
-
-def solve():
-    n = int(input())
-    dp,arr = [1]*n, []
-    for i in range(n):
-        a,b = map(int,input().split())
-        if a < b: a,b = b,a
-        arr.append((a,b))
-    arr.sort(reverse=True)
-
-    for i in range(1,n):    
-        for j in range(i):
-            if arr[i][1] <= arr[j][1]:
-                dp[i] = max(dp[i], dp[j] + 1)
-    print(max(dp))
-solve()
+n = int(input())
+dp,a = [1]*n, sorted([*sorted(map(int,input().split()))] for i in range(n))[::-1]
+for i in range(n):
+    for j in range(i+1, n):
+        if a[i][1] >= a[j][1]:
+            dp[j] = max(dp[j], dp[i] + 1)
+print(max(dp))
