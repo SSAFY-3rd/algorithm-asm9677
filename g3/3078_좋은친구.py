d2f211874efdd671bff7edaab61c912abaf61364
SungMin Ahn <asm9677@naver.com>
2021-01-16 20:02:11 +0900

    2021-01-15 알고리즘 문제 풀이

---
+++ g3/3078_좋은친구.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n,k = map(int, input().split())
+names = [0]*(n+k)
+
+for i in range(n):
+    names[i] = len(input())-1
+
+res = 0    
+count = [0]*21
+count[names[0]] = 1
+l,r = -k,0
+while l+1 < n:        
+    count[names[l]] -= 1
+    res += count[names[l]]
+    
+    l,r = l+1,r+1
+    count[names[r]] += 1
+print(res)      
